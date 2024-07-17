673ba22284205d6d1d1eb7a129a74bed08aa1bd3
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-07-17 09:14:23 +0000

    initial commit

--- map.js
+++ map.js
@@ -1,20 +1,32 @@
-  
-/**
- * To run this file in Gitpod, use the 
- * command node map.js in the terminal
- */
 
 // Using a for loop
-
+let numbers = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of numbers) {
+  results.push(num * 2);
+}
+console.log("Normal for loop: ", results);
+//-----------------------------------------
 
 // Using map()
-
+const multiply = function (num) {
+  return num * 2;
+} 
+const mapMethod = numbers.map(multiply);
+console.log("Using map method with function: ", mapMethod);
+//-----------------------------------------
 
 // Simplified w/ map()
-
+const mapMethodWithFunction = numbers.map(function multiply(num) {
+  return num * 2;
+});
+console.log("Using map method, Function directly passed as argument of map method: ", mapMethodWithFunction);
+//-----------------------------------------
 
 // Simplfied w/ map() + arrow function
-
+const mapMethodArrowFunction = numbers.map(num => num * 2);
+console.log("Using map method and arrow function: ", mapMethodArrowFunction)
+//-----------------------------------------
 
 // With objects:
 const students = [
@@ -37,3 +49,11 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+
+console.log(students)
+const studentArray = students.map(student => [student.name, student.id]);
+console.log("Map method with objects, return an array of array: ", studentArray)
+
+const studentObject = students.map(student => ({id:student.id, name:student.name}));
+console.log("Map method with objects, return an array of object: ", studentObject)
